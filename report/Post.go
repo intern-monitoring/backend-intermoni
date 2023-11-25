ac8114345bf780d0b0d547effaf3258c268c36e1
Fatwaff <fax.mp4@gmail.com>
2023-11-25 16:02:05 +0700

    Fix TambahReportByMahasiswa function and add mahasiswa_magang package import

--- report/Post.go
+++ report/Post.go
@@ -5,39 +5,25 @@ import (
 	"time"
 
 	intermoni "github.com/intern-monitoring/backend-intermoni"
+	"github.com/intern-monitoring/backend-intermoni/mahasiswa_magang"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func TambahReportByMahasiswa(idmahasiswamagang, iduser primitive.ObjectID, db *mongo.Database, insertedDoc intermoni.Report) error {
-	mahasiswa, err := intermoni.GetMahasiswaFromAkun(iduser, db)
+func TambahReportByMahasiswa(iduser primitive.ObjectID, db *mongo.Database, insertedDoc intermoni.Report) error {
+	mahasiswa_magang, err := mahasiswa_magang.GetMahasiswaMagangByMahasiswa(iduser, db)
 	if err != nil {
 		return err
 	}
-	mahasiswa_magang, err := intermoni.GetMahasiswaMagangFromID(idmahasiswamagang, db)
-	if err != nil {
-		return err
-	}
-	pembimbing, err := intermoni.GetPembimbingFromAkun(insertedDoc.Penerima.ID, db)
-	if err != nil {
-		return err
-	}
-	mentor, err := intermoni.GetMentorFromAkun(insertedDoc.Penerima.ID, db)
-	if err != nil {
-		return err
-	}
-	if mahasiswa.ID != mahasiswa_magang.Mahasiswa.ID {
-		return fmt.Errorf("kamu bukan mahasiswa magang ini")
-	}
-	if mahasiswa_magang.Status != 3 {
-		return fmt.Errorf("kamu belum memiliki pembimbing/mentor")
-	}
-	if pembimbing.ID != mahasiswa_magang.Pembimbing.ID && mentor.ID != mahasiswa_magang.Mentor.ID {
-		return fmt.Errorf("kamu tidak dapat memberikan report selain kepada pembimbing dan mentor kamu")
+	if mahasiswa_magang.Status != 1 {
+		return fmt.Errorf("kamu belum lolos seleksi")
 	}
 	if insertedDoc.Judul == "" || insertedDoc.Isi == "" || insertedDoc.Penerima.ID == primitive.NilObjectID {
 		return fmt.Errorf("mohon untuk melengkapi data")
+	}
+	if insertedDoc.Penerima.ID != mahasiswa_magang.Pembimbing.ID && insertedDoc.Penerima.ID != mahasiswa_magang.Mentor.ID {
+		return fmt.Errorf("kamu tidak dapat memberikan report selain kepada pembimbing dan mentor kamu")
 	}
 	data := bson.M{
 		"mahasiswamagang": bson.M{
